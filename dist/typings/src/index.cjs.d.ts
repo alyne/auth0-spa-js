92e07ca2f5defe4221c4edd100a07673c9ca9a43
Sri Hari Raju Penmatsa <pshraju@gmail.com>
2020-03-24 14:52:39 +0530

    Remove dist, to sanitize for PR

--- dist/typings/src/index.cjs.d.ts
+++
@@ -1,6 +0,0 @@
-import createAuth0Client, { Auth0Client } from './index';
-declare const _default: typeof createAuth0Client & {
-  Auth0Client: typeof Auth0Client;
-  createAuth0Client: typeof createAuth0Client;
-};
-export default _default;
